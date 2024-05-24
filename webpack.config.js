ee91c97c5f9a668f37199d2da3a4a01460c7694d
patrickgilmartin <144620815+patrickgilmartin@users.noreply.github.com>
2024-05-24 19:50:18 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+  mode: 'development'
 };
