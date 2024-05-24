2b4abccdfe06220c1e682b8c0cd433b71a524a22
patrickgilmartin <144620815+patrickgilmartin@users.noreply.github.com>
2024-05-24 13:28:36 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,53 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('background', 'wp7619435.png'); // Make sure to use the correct path to your image
+    this.load.spritesheet('mario', 'Mario.2.png', { frameWidth: 30, frameHeight: 60 }); // Adjust frame size as necessary
+}
+
+let mario;
+let cursors;
+
+function create() {
+    this.add.image(400, 300, 'background');
+    
+    mario = this.physics.add.sprite(70, 550, 'mario');
+    mario.setBounce(0.2);
+    mario.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        mario.setVelocityX(-160);
+        mario.setFlipX(true); // Flip sprite to face left
+    } else if (cursors.right.isDown) {
+        mario.setVelocityX(160);
+        mario.setFlipX(false); // Ensure sprite faces right
+    } else {
+        mario.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown && mario.body.touching.down) {
+        mario.setVelocityY(-330);
+    }
+}
