337ae78c8a2a5a720d0e9bf5c50aaa7baff46d1c
patrickgilmartin <144620815+patrickgilmartin@users.noreply.github.com>
2024-05-24 14:04:25 -0400

    Update game.js

--- game.js
+++ game.js
@@ -20,17 +20,16 @@ const game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('background', 'wp7619435.png'); 
-    this.load.spritesheet('mario', 'Mario.2.png', { frameWidth: 30, frameHeight: 60 });
-
+    this.load.spritesheet('mario', 'Mario.2.png', { frameWidth: 30, frameHeight: 60 }); 
 }
 
 let mario;
 let cursors;
 
 function create() {
-    this.add.image(400, 300, 'background');
+    this.add.image(640, 400, 'background'); 
     
-    mario = this.physics.add.sprite(70, 550, 'mario');
+    mario = this.physics.add.sprite(70, 750, 'mario');
     mario.setBounce(0.2);
     mario.setCollideWorldBounds(true);
 
@@ -40,10 +39,10 @@ function create() {
 function update() {
     if (cursors.left.isDown) {
         mario.setVelocityX(-160);
-        mario.setFlipX(true); // Flip sprite to face left
+        mario.setFlipX(true);
     } else if (cursors.right.isDown) {
         mario.setVelocityX(160);
-        mario.setFlipX(false); // Ensure sprite faces right
+        mario.setFlipX(false);
     } else {
         mario.setVelocityX(0);
     }
