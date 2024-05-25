3616d5f64c4aa2a7eb8e5acc162abf8265d52358
patrickgilmartin <144620815+patrickgilmartin@users.noreply.github.com>
2024-05-24 21:47:19 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
-  mode: 'development',
+  mode: 'production'
 };
