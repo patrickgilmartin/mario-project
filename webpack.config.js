e3bf4edeac8c30db1b900ad9623c2af867cc854c
patrickgilmartin <144620815+patrickgilmartin@users.noreply.github.com>
2024-05-24 20:08:08 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-        ],
-    },
-    mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  mode: 'development',
 };
