80a5c8c80d5de4dc540dd1d4879977ff2e18f1d8
patrickgilmartin <144620815+patrickgilmartin@users.noreply.github.com>
2024-05-24 22:28:11 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production'
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
