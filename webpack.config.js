264bf01bfd4731ead910bf54140433681dcd9fc7
patrickgilmartin <patrickgilmartin@users.noreply.github.com>
2024-05-25 03:24:09 +0000

    deploy: 3baac2762c66ff9bbe5d97ae415c1bd4ac78e769

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|wav)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: 'src',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
